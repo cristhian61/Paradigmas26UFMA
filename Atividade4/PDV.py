8de19a313468b3e288400119e042a72555044277
cristhian61 <ncristhianla@gmail.com>
2026-09-25 11:51:25 -0300

    Adicionado a função sacar(saldo)

--- Atividade4/PDV.py
+++ Atividade4/PDV.py
@@ -12,9 +12,8 @@ def menu():
     op = int(input("Escolha uma opção (1~4): "))
     return op
 
-
 def consultar_saldo(saldo):
-    print(f"\nSeu saldo é: {saldo:.2f}")
+    print(f"\nSeu saldo é: R$ {saldo:.2f}")
     input("Press Enter to continue...")
     
 def depositar(saldo):
@@ -22,8 +21,16 @@ def depositar(saldo):
     print("\nO valor foi depositado com sucesso!")
     return saldo
 
-#def sacar()
+def sacar(saldo):
+    
+    valor_sacar = float(input("\nDigite o valor a ser sacado: "))
 
+    if(valor_sacar <= saldo):
+        print("\nSaque realizado com sucesso")
+        return (saldo - valor_sacar)
+    else:
+        print("\n Valor indisponivel para saque")
+        return saldo
     
 saldo = 0
 
@@ -36,8 +43,9 @@ while True:
         case 2:
             saldo = depositar(saldo)
         case 3:
-            sacar()
+            saldo = sacar(saldo)
         case 4:
-            brack
+            print("Finalizando apliação...\n\n")
+            break
         case _:
             print("Valor invalido, por favor selecione uma opção válida (1~4).")
